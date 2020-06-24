6ce87856cf633cbf243bbdc020658ee1a6e1806f
sabri5club <sabri.tanich@gmail.com>
2020-06-24 16:11:19 +0200

    ee

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 
     app.get('*',(req,res) => {
-        res.sendFile(path.join(_dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'));
     });
 }
 
