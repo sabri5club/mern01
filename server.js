3fc99bd66e1ce5ae6a84dd2b63cacfe888cf231f
sabri5club <sabri.tanich@gmail.com>
2020-06-24 15:57:18 +0200

    test1

--- server.js
+++ server.js
@@ -23,8 +23,9 @@ const PORT = process.env.PORT || 8080;
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
+
     app.get('*',(req,res) => {
-        res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.join(_dirname, 'client', 'build', 'index.html'));
     });
 }
 
