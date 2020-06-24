422a50e0064005a5d8de392354e706b71ad117da
sabri5club <sabri.tanich@gmail.com>
2020-06-24 16:27:17 +0200

    ko

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ connectDB();
 //Init middleware
 app.use(express.json({extented:false}));
 
-app.get('/',(req,res) => res.send('Serveur node fonctionnel'));
+app.get('/api/auth',(req,res) => res.send('Serveur node fonctionnel'));
 
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
@@ -22,10 +22,11 @@ const PORT = process.env.PORT || 8080;
 
 
 if (process.env.NODE_ENV === 'production') {
+
     app.use(express.static('client/build'));
 
-    app.get('*',(req,res) => {
-        res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
 
